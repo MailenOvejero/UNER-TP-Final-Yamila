5a43e69f75c12089d9020993f875a7f876a827d6
MailenOvejero <yamilamailenovejero27@gmail.com>
2025-11-06 19:03:30 -0300

    Editamos cache.js

--- src/config/cache.js
+++ src/config/cache.js
@@ -1,27 +1,22 @@
 import apicache from 'apicache';
 
 // Inicializa el objeto de caché de apicache
-const cache = apicache.middleware;
-
-// Opciones de configuración por defecto
-const defaultOptions = {
+const cache = apicache.options({
     // 5 minutos por defecto, puede ser '10 minutes', '1 hour', etc.
     defaultDuration: '5 minutes', 
     statusCodes: {
         include: [200]
     },
-    header: 'X-APICACHE',// esta linea le dice a postman si la solicitud viene del server o de cache
-};
+    header: 'X-APICACHE' // esta linea le dice a postman si la solicitud viene del server o de cache
+}).middleware;
 
 /**
  * Función para obtener el middleware de caché que se usa en las rutas GET.
  * @param {string} duration Duración de caché específica (opcional).
- * @param {object} options Opciones de configuración adicionales (opcional).
  * @returns {function} Middleware de apicache configurado.
  */
-export const cacheMiddleware = (duration = defaultOptions.defaultDuration, options = {}) => {
-    const finalOptions = { ...defaultOptions, ...options };
-    return cache(duration, finalOptions);
+export const cacheMiddleware = (duration = '5 minutes') => {
+    return cache(duration);
 };
 
 // Exporta el objeto apicache original para funciones de limpieza (.clear(), .getIndex())
